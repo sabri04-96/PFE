cb95a86df1a625dc32819ed715539b2780dfbb43
sabri04-96 <sabri.fetoui@tek-up.de>
2022-03-07 09:32:35 +0100

    Add all my files

---
+++ pfe/src/test/java/com/projetfinetude/pfe/PfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetfinetude.pfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
